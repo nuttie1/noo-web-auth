8b8e30e2d12977dcaea451ecd2e539d8ee646794
nuttie1 <otto.keksi@gmail.com>
2024-03-04 13:52:32 +0200

    Update database URL in db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const mongoConnect = async () => {
-  const connection = await mongoose.connect(process.env.DATABASE_URL as string);
+  const connection = await mongoose.connect(process.env.DB_URL as string);
   console.log('DB connected successfully');
   return connection;
 };
